c1da1bf86bfcf03065b651bd140f5bbc9a251e51
Yoshiyasu KO <te108128@gmail.com>
2023-01-30 00:25:24 +0900

    implement model: Landmark

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,38 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Yoshiyasu KO on 2023/01/29.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    fileprivate var imageName: String
+    fileprivate var coordinates: Coordinates
+    var state: String
+    var park: String
+    
+    enum Category: String, CaseIterable, Hashable, Codable {
+        case featured = "Featured"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+    }
+}
+
+extension Landmark {
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+    
+    var image: Image {
+        ImageStore.shared.image(name: imageName)
+    }
+}
