31b4ec29614662ea3e2997d3ed732c37170136d2
Yoshiyasu KO <te108128@gmail.com>
2023-01-30 01:42:11 +0900

    implement LandmarkRow

---
+++ Landmarks/LandmarkRow.swift
@@ -0,0 +1,33 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Yoshiyasu KO on 2023/01/30.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+            LandmarkRow(landmark: landmarkData[2])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
