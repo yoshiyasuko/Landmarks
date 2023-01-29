e63bc215fd19e46164cc669184e01fb5e67277c7
Yoshiyasu KO <te108128@gmail.com>
2023-01-30 00:23:39 +0900

    implement model: Coordinates

---
+++ Landmarks/Model/Coordinates.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinates.swift
+//  Landmarks
+//
+//  Created by Yoshiyasu KO on 2023/01/29.
+//
+
+import SwiftUI
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
